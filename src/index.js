85690f59be009620575770604f1fbbf69c78bbf4
belveter <dmitry.nayd@gmail.com>
2022-01-13 23:45:18 +0300

    feat: reverse number done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+  str=n.toString();
+  let rez = '';
+  for(i=str.length - 1; i >= 0; i--){
+      rez += str[i];
+  }
+  rez = parseInt(rez);
+  return rez;
 }
